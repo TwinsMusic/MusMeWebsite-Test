ccd61f17a346ffc88a4e9e81c99f2e3b35948e92
izzyphobe <izzy@alfheim.org>
2022-02-04 15:49:30 -0600

    waveform looks nicer

--- src/src/Song.js
+++ src/src/Song.js
@@ -13,17 +13,45 @@ class Song extends React.Component  {
         barHeight: 1,
         fillParent: true,
         height: 75,
+        barWidth: 3,
+        responsive:true,
+        hideScrollbar: true,
+        barRadius: 1,
       }),
+      container: document.getElementById(props.container),
     }
     console.log(this.props.name);
     this.state.wavesurfer.load(this.props.file);
+    //this.state.container.style.display = 'none';
+    console.log(this.state.container.style.display);
 
+  }
+
+  buttonClick(){
+      this.setState({active: true});
+      this.state.wavesurfer.playPause();
+      this.state.container.style.display = "block";
+      this.props.onPlay(this);
+      console.log(this.props.playlist);
+  }
+
+  closeContainer(){
+      this.state.container.style.display = "none";
+  }
+
+  stop(){
+    this.state.wavesurfer.stop();
   }
   
   render(){
-    return <button id = {this.props.buttonID} class = "button" onClick={() => {this.state.wavesurfer.playPause();}}>Play/Pause</button>
-  }
-
+    return(
+      <div>
+        <button id = {this.props.buttonID} class = "button" onClick={() => { this.buttonClick();}}>Play/Pause</button>
+        <em>{this.props.name}</em>
+      </div>
+    )
+  } 
+    
 }
 
 export default Song;
