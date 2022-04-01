becc2cf8d32cdab16513a5d3fedfa8675fee2752
Phoebe Babineau <pbabinea@trinity.edu>
2022-04-01 10:36:59 -0500

    add terms page
    not showing up yet

---
+++ src/src/Terms.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Terms() {
+  return (
+    <section class="section">
+    <div class="container">
+        <h1 class = "has-text-centered">Terms Of Use</h1>
+        <br></br>
+        <p class="subtitle is-5"><b>ANNUAL SUBSCRIPTION AGREEMENT</b></p>
+        <p class="subtitle is-5">What You Get From Our MusMe Subscription<br></br>
+        The moment you subscribe with TwinsMusic, you are agreeing with the following terms and conditions.
+        </p>
+        <p class="subtitle is-5"><b>GUARANTEE</b></p>
+        <p class="subtitle is-5">TwinsMusic Enterprises (TME) guarantees that it owns and/or controls the rights represented herein with respect to the recordings and the musical compositions in the Work and has and will hold throughout the TERRITORY and during the DURATION of the subscription period, and above listed rights to exploit the Work as contemplated herein.</p>
+        <p class="subtitle is-5"><b>TERMS OF USE (explained)</b></p>
+        <p class="subtitle is-5">All audio files are royalty-free. This means that there are no additional fees owed for small content creators like YouTubers, film students, personal videos, small business owners. Once you have downloaded the audio file, you have the license to use that sound in the year of the subscription period as many times as you want, and you will never be required to pay another fee of any kind. However, the audio files in the library are not allowed to be used for commercial use, video games, film, advertising, and any other commercial production. For this kind of purpose, you can grant a specific license from TwinsMusic Enterprises.</p>
+        <p class="subtitle is-5"><b>RIGHTS NOT INCLUDED IN THIS AGREEMENT</b></p>
+        <p class="subtitle is-5">The rights granted to the subscriber do not permit the subscriber to (1) claim authorship of the music represented under this AGREEMENT; (2) transfer, share or sub-lease this license agreement with any other party; (3) copy or duplicate the Work except for the granted use in the terms of use; (4) permit any other individual or third party the right to use the Work in place of the subscriber; (5) resell, trade, or exploit for profit the Work contained herein outright or as part of other music and/or audio-related collections, in part or in whole, to any other individual or party.</p>
+        <p class="subtitle is-5">Audio files cannot be used, copied, resold or distributed as individual files.</p>
+        <p class="subtitle is-5">You may use the tracks, samples and loops in:<br></br>
+        • individual videos (YouTube, Vimeo, social media platforms)<br></br>
+        • podcasts<br></br>
+        • demos<br></br>
+        • school projects<br></br>
+        • any other type of audio/visual projects
+        </p>
+        <p class="subtitle is-5">You may NOT use the tracks, samples and loops in:<br></br>
+        • film<br></br>
+        • games<br></br>
+        • apps<br></br>
+        • concert performances<br></br>
+        • TV or radio advertisements<br></br>
+        • software<br></br>
+        • commercial musical works or in other songs (pop songs etc.) based on our audio files (derivative work)
+        </p>
+        <p class="subtitle is-5">The viewing, printing or downloading of any content from the website can only be used for private use and not for commercial purposes (for purchased or free downloaded sounds, please see the terms of use above).</p>
+   </div>
+    </section>
+    
+  );
+}
+
+export default Terms;
